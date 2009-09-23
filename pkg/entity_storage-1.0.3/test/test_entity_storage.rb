14c5d255d4fd0953e2c89d9edf7c68485934e81f
Joshua Siler <joshua.siler@gmail.com>
2009-09-23 16:57:46 -0700

    update

--- pkg/entity_storage-1.0.3/test/test_entity_storage.rb
+++ pkg/entity_storage-1.0.3/test/test_entity_storage.rb
@@ -8,49 +8,49 @@ class TestEntityStorage < Test::Unit::TestCase
   "new_jobs_last_run" => DateTime.parse("1-1-1900"),
   "email_test_address" => "joshuas@bnj.com" }
   
-  attr_accessor :entity_storage
+  attr_accessor :EntityStore
   
   def setup
     ActiveRecord::Base.establish_connection(YAML::load(File.read(File.dirname(__FILE__) + '/../config/database.yml'))) unless ActiveRecord::Base.connected?
     ActiveRecord::Base.connection.execute("delete from entity_storage")
-    self.entity_storage ||= EntityStorage::Storage.new(DEFAULT_KEYS) 
+    EntityStore = EntityStorage::Storage.new(DEFAULT_KEYS) if EntityStore.nil?
   end
   
   # tests value setting and getting functionality, along with default creation  
   def test_defaultkeys
     DEFAULT_KEYS.each { |key,value| 
-      self.entity_storage.delete(key)
+      self.EntityStore.delete(key)
       
       # set key when it doesn't exist, should go to default
-      e = self.entity_storage[key]
+      e = self.EntityStore[key]
       assert_equal e, value
       
-      self.entity_storage.delete(key)
+      self.EntityStore.delete(key)
       
       # set and try method missing access
       if key[/\w/] == key
-				eval("e = self.entity_storage."+key)
+				eval("e = self.EntityStore."+key)
 				assert_equal e, value
 			end
       
       # change it to something else
-      self.entity_storage[key] = Time.now.to_s
-      e = self.entity_storage[key]
+      self.EntityStore[key] = Time.now.to_s
+      e = self.EntityStore[key]
       assert_not_equal e, value
       
       # find out it's default
-      e = self.entity_storage.default(key)
+      e = self.EntityStore.default(key)
       assert_not_equal e, value
       
       # set it back to default
-      self.entity_storage.default!(key)
-      e = self.entity_storage[key]
+      self.EntityStore.default!(key)
+      e = self.EntityStore[key]
       assert_equal e, value
       
       # set it to something else using method missing
       if key[/\w/] == key
-				eval("e = self.entity_storage."+key +" = Time.now.to_s")
-				eval("e = self.entity_storage."+key)
+				eval("e = self.EntityStore."+key +" = Time.now.to_s")
+				eval("e = self.EntityStore."+key)
 				assert_equal e, value
       end 
     }
@@ -58,41 +58,41 @@ class TestEntityStorage < Test::Unit::TestCase
   
   # tests setting values when no DEFAULT_KEY exists
   def test_settingvalues
-    self.entity_storage.delete('test')
+    self.EntityStore.delete('test')
     
     # key that can't be accessed via method missing
     # set value and check it
-    self.entity_storage['big long key that cannot be accessed via method missing'] = 'what up'
-    e = self.entity_storage['big long key that cannot be accessed via method missing']
+    self.EntityStore['big long key that cannot be accessed via method missing'] = 'what up'
+    e = self.EntityStore['big long key that cannot be accessed via method missing']
     assert_equal e, 'what up'
     
     # set another value and check it
-    self.entity_storage['big long key that cannot be accessed via method missing'] = 'another value'
-    e = self.entity_storage['big long key that cannot be accessed via method missing']
+    self.EntityStore['big long key that cannot be accessed via method missing'] = 'another value'
+    e = self.EntityStore['big long key that cannot be accessed via method missing']
     assert_equal e, 'another value'
     
-    self.entity_storage['test'] = 'what up'
-    e = self.entity_storage['test']
+    self.EntityStore['test'] = 'what up'
+    e = self.EntityStore['test']
     assert_equal e, 'what up'
     
-    self.entity_storage['test'] = 'another value'
-    e = self.entity_storage['test']
+    self.EntityStore['test'] = 'another value'
+    e = self.EntityStore['test']
     assert_equal e, 'another value'
     
     # try the method missing version of the tests
-    e = self.entity_storage.test = 'what up'
-    e = self.entity_storage.test
+    e = self.EntityStore.test = 'what up'
+    e = self.EntityStore.test
     assert_equal e, 'what up'
     
-    e = self.entity_storage.test = 'another value'
-    e = self.entity_storage.test
+    e = self.EntityStore.test = 'another value'
+    e = self.EntityStore.test
     assert_equal e, 'another value'
     
   end
   
   def test_getunknownvalue
-    self.entity_storage.delete('does not exist')
-    e = self.entity_storage['does not exist']
+    self.EntityStore.delete('does not exist')
+    e = self.EntityStore['does not exist']
     assert_nil e
   end
   
